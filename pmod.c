630b8730495bd118e89876054b28abecc794fd5f
jankowskib <jankowskib@msoe.edu>
2025-10-31 12:02:02 -0500

    Implemented pmod.c

--- pmod.c
+++ pmod.c
@@ -1,9 +1,18 @@
-// Name, etc
-
-// pmod.c
+/* Benjamin Jankowski
+ * Lab 9
+ * pmod.c
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
 
 int main(int argc, char* argv[])
 {
+    nice(10); // https://www.man7.org/linux/man-pages/man2/nice.2.html
 
+    struct timespec sleep_time = {1, 837272638};
+    nanosleep(&sleep_time, NULL); // https://man7.org/linux/man-pages/man2/nanosleep.2.html
+
+    printf("Goodbye\n");
     return 0;
 }
